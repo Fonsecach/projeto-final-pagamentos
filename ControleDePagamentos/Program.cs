97c9e0ef47cf76b4b3a8ffd24ccf3618005db48d
Cleiton H. da Fonseca <chdf@proton.me>
2024-05-05 16:02:46 -0300

    Implementação do Metodo Get e Post e do arquivo de testes ControleDePagamentos.http

--- ControleDePagamentos/Program.cs
+++ ControleDePagamentos/Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDataContext>();
 
 var app = builder.Build();
 
@@ -16,29 +26,44 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapPost("/api/pessoas/cadastrar", async ([FromBody] List<Pessoa> pessoas, [FromServices] AppDataContext contextPessoas) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var pessoasNaoSalvas = new List<Pessoa>();
 
-app.MapGet("/weatherforecast", () =>
+    foreach (var pessoa in pessoas)
+    {
+        // Verifica se a pessoa já está na base de dados pelo ID ou NumDocumento
+        var pessoaExistente = await contextPessoas.Pessoas.FirstOrDefaultAsync(p => p.ID == pessoa.ID || p.NumDocumento == pessoa.NumDocumento);
+
+        if (pessoaExistente != null)
+        {
+            // Pessoa já está na base de dados, então retorna erro
+            return Results.Conflict($"A pessoa com ID {pessoa.ID} ou NumDocumento {pessoa.NumDocumento} já está cadastrada.");
+        }
+        else
+        {
+            // Pessoa não está na base de dados, então salva
+            contextPessoas.Pessoas.Add(pessoa);
+            pessoasNaoSalvas.Add(pessoa);
+        }
+    }
+
+    await contextPessoas.SaveChangesAsync();
+
+    return Results.Created("", pessoasNaoSalvas);
+
+}).WithName("AddPessoas").WithOpenApi();
+
+app.MapGet("/api/pessoas/exibir", async ([FromServices] AppDataContext contextPessoas) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    var pessoas = await contextPessoas.Pessoas.ToListAsync();
+    if (pessoas.Any()){
+        var pessoasOrdenadas = pessoas.OrderBy(p => p.Nome).ToList();
+        return Results.Ok(pessoasOrdenadas);
+    }
+    return Results.NotFound("Nenhuma pessoa foi registrada");
+
+}).WithName("ListarPessoas").WithOpenApi();
+
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
